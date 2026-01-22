163fcc835b25dab7a4dd067b24fb6dc35c36dab7
whiteedeesign <167991584+whiteedeesign@users.noreply.github.com>
2026-01-22 03:07:35 +0300

    Define __dirname for ESM module compatibility
    Manually define __dirname for ESM modules to ensure compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+// Fix: Define __dirname manually for ESM modules as it is not available globally
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
